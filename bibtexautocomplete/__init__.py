5e7a5036b01283c70638ce29bf6074574abc52e5
Dorian Lesbre <dorian.lesbre@gmail.com>
2022-03-15 22:08:19 +0100

    RENAME script + update setup.py

--- bibtexautocomplete/__init__.py
+++ bibtexautocomplete/__init__.py
@@ -1,10 +1,11 @@
 from .core.autocomplete import BibtexAutocomplete
 from .core.main import main
-from .utils.constants import AUTHOR, EMAIL, VERSION_STR
+from .utils.constants import AUTHOR, EMAIL, URL, VERSION_STR
 
 __author__ = AUTHOR
 __email__ = EMAIL
 __version__ = VERSION_STR
+__url__ = URL
 
 __description__ = "Module to complete bibtex files by polling online databases"
 
--- setup.py
+++ setup.py
@@ -3,22 +3,25 @@ setup file. run 'python3 setup.py install' to install.
 """
 from setuptools import setup  # type: ignore
 
+import bibtexautocomplete
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="bibtexautocomplete",
-    version="0.2",
-    author="Dorian Lesbre",
-    url="https://github.com/dlesbre/bibtex-autocomplete",
-    description="Python package to autocomplete bibtex bibliographies ",
+    name=bibtexautocomplete.__name__,
+    version=bibtexautocomplete.__version__,
+    author=bibtexautocomplete.__author__,
+    author_email=bibtexautocomplete.__email__,
+    url=bibtexautocomplete.__url__,
+    description=bibtexautocomplete.__description__,
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=["bibtexautocomplete"],
-    scripts=["scripts/bibtexautocomplete"],
+    scripts=["scripts/btac"],
     install_requires=[
         "bibtexparser",
-        "git+https://github.com/dlesbre/alive-progress.git",
+        "alive-progress @ git+https://github.com/dlesbre/alive-progress.git",
     ],
     extras_require={
         "dev": ["pre-commit", "pytest", "mypy", "black", "flake8", "isort"],
@@ -26,12 +29,28 @@ setup(
     python_requires=">=3.9",
     license="MIT",
     platforms=["any"],
+    keywords=["bibtex biblatex latex autocomplete btac"],
     classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "Natural Language :: English",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Markup :: LaTeX",
         "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Typing :: Typed",
     ],
+    data_files=[("", ["LICENSE"])],
 )
