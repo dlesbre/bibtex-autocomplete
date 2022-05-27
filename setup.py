d6f7e20db8478100061cca1b14e3f2d8e9751302
Dorian Lesbre <dorian.lesbre@gmail.com>
2022-05-27 14:15:33 +0200

    Removed wheel from dev dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "alive-progress>=2.4.0",
     ],
     extras_require={
-        "dev": ["pre-commit", "pytest", "mypy", "black", "flake8", "isort", "wheel"],
+        "dev": ["pre-commit", "pytest", "mypy", "black", "flake8", "isort"],
     },
     python_requires=">=3.8",
     license="MIT",
